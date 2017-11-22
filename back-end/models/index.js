87f85997bb4b22b7dafaa67c5ffccc65c64cca4d
Ozgun Bal <ozgunbal@gmail.com>
2017-11-22 09:34:14 +0300

    fixed missing schemas

--- back-end/models/index.js
+++ back-end/models/index.js
@@ -1,5 +1,7 @@
 var models = {
     Categories: require('./categories.model'),
+    ProductType: require('./productType.model'),
+    Episode: require('./episode.model'),
     Movie: require('./movie.model'),
     Series: require('./series.model'),
     User: require('./user.model')
